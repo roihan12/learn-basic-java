5dfd219e10f1e97ce2e507ae9bc5310b8227b595
Your Name <roihansori12@gmail.com>
2024-10-08 23:33:28 +0700

    learn: tipe data char

---
+++ src/TypeChar.java
@@ -0,0 +1,14 @@
+public class TypeChar {
+    public static void main(String[] args) {
+        char e = 'E';
+        char k = 'K';
+        char o = 'O';
+
+        String apa = "ini";
+
+        System.out.print(apa);
+        System.out.print(e);
+        System.out.print(k);
+        System.out.print(o);
+    }
+}
